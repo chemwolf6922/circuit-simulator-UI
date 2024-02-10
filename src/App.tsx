feca4eccf09a6e73a0be5a646c9927ad061fd497
Feng Wang <wang6922@126.com>
2024-02-10 10:57:46 +0800

    move screen

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,12 @@ class App extends React.Component {
   #chipMoving = false;
   #movingChip = '';
   #chipMovingOffset = {x:0,y:0};
+  #listenersSet = false;
+  #spaceDown = false;
+  #screenMoving = false;
+  #screenMovingOffset = {x:0,y:0};
   state:{
+    canvasPosition:{x:number,y:number};
     chips:{[id:string]:{
       position:{x:number,y:number};
       chip:Chip;
@@ -65,6 +70,7 @@ class App extends React.Component {
       pinB:string;
     }>;
   } = {
+    canvasPosition:{x:0,y:0},
     chips:{
       'source':{
         position:{x:100,y:100},
@@ -85,18 +91,29 @@ class App extends React.Component {
       y:e.screenY-this.state.chips[id].position.y
     };
   }
-  onMouseUp(e:React.MouseEvent){
+  onMouseDown(e:MouseEvent){
+    if(this.#spaceDown){
+      this.#screenMoving = true;
+      this.#screenMovingOffset = {
+        x:e.screenX-this.state.canvasPosition.x,
+        y:e.screenY-this.state.canvasPosition.y
+      };
+    }
+  }
+  onMouseUp(e:MouseEvent){
     if(this.#chipMoving){
       this.#chipMoving = false;
     }
+    if(this.#screenMoving){
+      this.#screenMoving = false;
+    }
   }
-  onMouseMove(e:React.MouseEvent){
+  onMouseMove(e:MouseEvent){
     if(this.#chipMoving){
       e.preventDefault();
       e.stopPropagation();
       const x = e.screenX-this.#chipMovingOffset.x;
       const y = e.screenY-this.#chipMovingOffset.y;
-      console.log(x,y);
       this.setState({
         chips:{
           ...this.state.chips,
@@ -108,11 +125,47 @@ class App extends React.Component {
       });
       return;
     }
+    if(this.#screenMoving){
+      e.preventDefault();
+      e.stopPropagation();
+      /** @todo move the screen */
+      const x = e.screenX-this.#screenMovingOffset.x;
+      const y = e.screenY-this.#screenMovingOffset.y;
+      this.setState({
+        canvasPosition:{x,y}
+      });
+      return;
+    }
+  }
+  onKeyDown(e:KeyboardEvent){
+    if(e.key === ' '){
+      this.#spaceDown = true;
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  }
+  onKeyUp(e:KeyboardEvent){
+    if(e.key === ' '){
+      this.#spaceDown = false;
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  }
+  componentDidMount(): void {
+    if(this.#listenersSet) {
+      return;
+    }
+    this.#listenersSet = true;
+    window.addEventListener('mousedown',this.onMouseDown.bind(this));
+    window.addEventListener('mousemove',this.onMouseMove.bind(this));
+    window.addEventListener('mouseup',this.onMouseUp.bind(this));
+    window.addEventListener('keydown',this.onKeyDown.bind(this));
+    window.addEventListener('keyup',this.onKeyUp.bind(this));
   }
   render() {
     return (
       <div className="App">
-        <div className='DrawingArea' onMouseMove={this.onMouseMove.bind(this)} onMouseUp={this.onMouseUp.bind(this)}>
+        <div className='Canvas' style={{left:this.state.canvasPosition.x,top:this.state.canvasPosition.y}}>
           {Object.entries(this.state.chips).map(([n,c])=>
             <div key={n} style={{left:c.position.x,top:c.position.y}} className='ChipContainer'>
               <ChipUI {...{id:n,info:c.chip.info,hooks:{
@@ -120,6 +173,7 @@ class App extends React.Component {
               }}} />
             </div>
           )}
+          {/* @todo use a div to stretch the drawing area (to extend the size when the user moves the window) */}
         </div>
       </div>
     );
